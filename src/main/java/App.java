93bda17d67d0a9320d9429d9a4462db30263bde7
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:42:56 +0300

    Задание выполнено

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
+        ApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) context.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) context.getBean("helloworld");
+        System.out.println(bean == bean1);
+
+        Cat catBean = (Cat) context.getBean("cat");
+        Cat catBean1 = (Cat) context.getBean("cat");
+        System.out.println(catBean == catBean1);
     }
 }
